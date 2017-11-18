e81e729d9687b0d8c299d250fea0877837cde4e0
fortunto2 <info@3dstr.ru>
2017-11-19 02:53:16 +0500

    Метрика, подсчет адекватных слов сканера

--- ocr.py
+++ ocr.py
@@ -71,31 +71,33 @@ def cropImage(image_input):
     return image_out
 
 
+# @decor_function_call
+# def binarImage(image_input):
+#     """
+#     Биномиризация картинки, ч.б.
+#     python lib/process_image.py out/2.crop.png out/3.binar.png
+#     """
+#
+#     image_out = dir_out + '3.binar.png'
+#
+#     # ТОДО переписать на функцию
+#     os.system("lib/process_image.py {} {}".format(image_input, image_out))
+#
+#     return image_out
+
+
 @decor_function_call
-def binarImage(image_input):
-    """
-    Биномиризация картинки, ч.б.
-    python lib/process_image.py out/2.crop.png out/3.binar.png
-    """
-
-    image_out = dir_out + '3.binar.png'
-
-    # ТОДО переписать на функцию
-    os.system("lib/process_image.py {} {}".format(image_input, image_out))
-
-    return image_out
-
-
-@decor_function_call
-def textCleaner(image_input):
+def cleanImage(image_input):
     """
     Используем шел скрипт textcleaner
     http://www.fmwconcepts.com/imagemagick/textcleaner/index.php
     """
 
-    image_out = dir_out + '3.clean.png'
+    image_out = dir_out + '3.2.clean.png'
+    # options = ' -g -e stretch -f 25 -o 20 -t 30 -u -s 1 -T -p 20 '
+    options = ' -T -p 20 '
 
-    os.system("sh lib/textcleaner {} {}".format(image_input, image_out))
+    os.system("sh lib/textcleaner {} {} {}".format(options, image_input, image_out))
 
     return image_out
 
@@ -108,13 +110,21 @@ def extractTextTesseract(image_input):
     from PIL import Image
     import pytesseract
 
-    # def countWords(text):
+    def countWords(text):
+        "Чистим текст и остовляем слова больше 2х символов"
+        import re
+        clear = re.findall(u"[а-яА-Яa-zA-Z]+", text, re.UNICODE)
+        clear_big = [s for s in clear if len(s) > 2]
+        clear_big = sorted(clear_big)
+
+        print(json.dumps(clear_big,  sort_keys=True, ensure_ascii=False))
+        clear_len = len(clear_big)
+        print (Fore.YELLOW + 'Количество слов: ' + str(clear_len))
+        return clear_big, clear_len
 
     out_text = pytesseract.image_to_string(Image.open(image_input), lang='rus')
-    out_text = out_text.replace('\n\n','\n')
-    print (out_text)
 
-    return out_text
+    return out_text, countWords(out_text)
 
 
 def rescaleImage(image_input, width=1000):
@@ -123,7 +133,7 @@ def rescaleImage(image_input, width=1000):
     convert example.png -resize 200 example.png
     """
 
-    image_out = dir_out + 'rescale.png'
+    image_out = dir_out + '3.1.rescale.png'
 
     os.system("convert {} -resize {} -auto-level {}".format(image_input, width, image_out))
 
@@ -152,6 +162,8 @@ if __name__ == '__main__':
             shutil.rmtree(dir_out)
         os.makedirs(dir_out)
 
+        os.system("convert {} {}".format(image_input, dir_out + '0.original.png'))
+
         # начинаем обработку
         # rescaleInput = rescaleImage(image_input, width=2000)
 
@@ -164,6 +176,6 @@ if __name__ == '__main__':
         text = extractTextTesseract(rescaled)
         print (len(text))
 
-        image3step = textCleaner(image_input=rescaled)
-        text = extractTextTesseract(image3step)
+        image3step = cleanImage(image_input=rescaled)
+        text, text_clear = extractTextTesseract(image3step)
         print (len(text))
